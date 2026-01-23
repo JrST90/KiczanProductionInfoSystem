c58689778ccadddbc63dfa0db1461da6748077df
Josh Stanczyk <sta88217@pennwest.edu>
2026-01-23 12:51:56 -0500

    Sprint 1: Initial Application State, add designated buttons to interface

--- KiczanProductionInfoSystem/Form1.Designer.cs
+++ KiczanProductionInfoSystem/Form1.Designer.cs
@@ -29,12 +29,270 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.updateRecordToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteRecordArchiveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.restoreRecordMainTableToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeight = 46;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 182);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(2165, 740);
+            this.dataGridView1.TabIndex = 0;
+            //TO DO: Add event handlers for dataGridView1
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Search by Due Date Range",
+            "Search by Part Number",
+            "Search by Operator Name",
+            "Search by Fabrication Department",
+            "Bending Work Query",
+            "NC Machine Work Query",
+            "Last 7 Days",
+            "Last 14 Days"});
+            this.comboBox1.Location = new System.Drawing.Point(25, 38);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(431, 33);
+            this.comboBox1.TabIndex = 13;
+            //TO DO: Add event handler for comboBox1
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(1605, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 51);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = false;
+            //TO DO: Add event handler for button1
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(711, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(254, 31);
+            this.textBox1.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(706, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 25);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Search Value";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 17;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(462, 38);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(225, 33);
+            this.comboBox2.TabIndex = 18;
+            //TO DO: Add event handler for comboBox2 if needed
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(1007, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(372, 155);
+            this.label3.TabIndex = 19;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(63, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 25);
+            this.label4.TabIndex = 20;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Blue;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(1803, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(192, 51);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Create Record";
+            this.button2.UseVisualStyleBackColor = false;
+            //TO DO: Add event handler for button2
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Purple;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(2001, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(192, 50);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "Export";
+            this.button3.UseVisualStyleBackColor = false;
+            //TO DO: Add event handler for button3
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1798, 139);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 25);
+            this.label5.TabIndex = 23;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1996, 139);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 25);
+            this.label6.TabIndex = 24;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(2057, 948);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(133, 46);
+            this.button4.TabIndex = 25;
+            this.button4.Text = "Next";
+            this.button4.UseVisualStyleBackColor = true;
+            //TO DO: Add event handler for button4
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1919, 948);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(132, 46);
+            this.button5.TabIndex = 26;
+            this.button5.Text = "Previous";
+            this.button5.UseVisualStyleBackColor = true;
+            //TO DO: Add event handler for button5
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.updateRecordToolStripMenuItem,
+            this.deleteRecordArchiveToolStripMenuItem,
+            this.restoreRecordMainTableToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(385, 118);
+            // 
+            // updateRecordToolStripMenuItem
+            // 
+            this.updateRecordToolStripMenuItem.Name = "updateRecordToolStripMenuItem";
+            this.updateRecordToolStripMenuItem.Size = new System.Drawing.Size(384, 38);
+            this.updateRecordToolStripMenuItem.Text = "Update Record";
+            //TO DO: Add event handler for updateRecordToolStripMenuItem
+            // 
+            // deleteRecordArchiveToolStripMenuItem
+            // 
+            this.deleteRecordArchiveToolStripMenuItem.Name = "deleteRecordArchiveToolStripMenuItem";
+            this.deleteRecordArchiveToolStripMenuItem.Size = new System.Drawing.Size(384, 38);
+            this.deleteRecordArchiveToolStripMenuItem.Text = "Delete Record (Archive)";
+            // 
+            // restoreRecordMainTableToolStripMenuItem
+            // 
+            this.restoreRecordMainTableToolStripMenuItem.Name = "restoreRecordMainTableToolStripMenuItem";
+            this.restoreRecordMainTableToolStripMenuItem.Size = new System.Drawing.Size(384, 38);
+            this.restoreRecordMainTableToolStripMenuItem.Text = "Restore Record (Main Table)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(2205, 1006);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem updateRecordToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteRecordArchiveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem restoreRecordMainTableToolStripMenuItem;
     }
 }
 
