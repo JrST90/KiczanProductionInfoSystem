f1ae764c7e48fd721ded5fd5dd193fcc44e62283
Lorvanos2200 <avarice95@gmail.com>
2026-01-29 12:03:20 -0500

    due date validation
    The code to be reviewed for due date validation

--- KiczanProductionInfoSystem/DataValidation.cs
+++ KiczanProductionInfoSystem/DataValidation.cs
@@ -6,6 +6,72 @@ namespace KiczanProductionInfoSystem
 {
     internal class DataValidation
     {
+        //Validate date range input.
+        internal bool validateDateRange(string input)
+        {
+            //Regular expression to check format with included hyphen.
+            string pattern = @"\d{2}\/\d{2}\/\d{4}\-\d{2}\/\d{2}\/\d{4}";
+            Regex rg = new Regex(pattern);
+            bool matchesPattern = false;
+            bool isValidOne = false;
+            bool isValidTwo = false;
+            bool dateValid = false;
+
+            if (rg.IsMatch(input))
+            {
+                matchesPattern = true;
+            }
+            else
+            {
+                matchesPattern = false;
+            }
+
+            //Check to make sure provided dates are logically sound.
+            if (matchesPattern)
+            {
+                string format = "MM/dd/yyyy";
+                string[] dateArray = input.Split('-');
+                DateTime dtOne;
+                DateTime dtTwo;
+                isValidOne = DateTime.TryParseExact(dateArray[0], format, CultureInfo.InvariantCulture, DateTimeStyles.None, out dtOne);
+                isValidTwo = DateTime.TryParseExact(dateArray[1], format, CultureInfo.InvariantCulture, DateTimeStyles.None, out dtTwo);
+            }
+
+            if(isValidOne && isValidTwo)
+            {
+                dateValid = true;
+            }
+            else
+            {
+                dateValid = false;
+            }
+            return dateValid;
+        }
+
+        //Validate date range, ensuring beginning date occurs before end date.
+        internal bool validateDateRangeBegBeforeEnd(string input)
+        {
+            bool dateValid = false;
+            string format = "MM/dd/yyyy";
+            string[] dateArray = input.Split('-');
+            DateTime dtOne;
+            DateTime dtTwo;
+
+            DateTime.TryParseExact(dateArray[0], format, CultureInfo.InvariantCulture, DateTimeStyles.None, out dtOne);
+            DateTime.TryParseExact(dateArray[1], format, CultureInfo.InvariantCulture, DateTimeStyles.None, out dtTwo);
+
+            if(dtOne < dtTwo)
+            {
+                dateValid = true;
+            }
+            else
+            {
+                dateValid = false;
+            }
+
+            return dateValid;
+        }
+        
         //Validate part number input.
         internal bool validatePartNumber(string input)
         {
