269b4a2071bcd15df37c4afe50bccaf1b04e95d6
Josh S <JoshStanczyk@gmail.com>
2026-06-19 14:40:50 -0400

    Fix: Updated Export feature to work with modern .NET 8.0.

--- KiczanProductionInfoSystem/Form1.cs
+++ KiczanProductionInfoSystem/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace KiczanProductionInfoSystem
 {
