76461a250e25e66bbb61f0cf06236fc34fb47018
Josh S <JoshStanczyk@gmail.com>
2026-07-15 15:20:24 -0400

    Fix: Removed redundant using statements in CreateRecord.cs and Form1.cs.

--- KiczanProductionInfoSystem/CreateRecord.cs
+++ KiczanProductionInfoSystem/CreateRecord.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Globalization;
 using System.Windows.Forms;
-using System.Threading.Tasks;
 
 namespace KiczanProductionInfoSystem
 {
--- KiczanProductionInfoSystem/Form1.cs
+++ KiczanProductionInfoSystem/Form1.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
-using Excel = Microsoft.Office.Interop.Excel;
 
 namespace KiczanProductionInfoSystem
 {
